656debbe9cc0c8d4ed1eb805fcc9a82215ba12b2
Grant <22186487+grantwill74@users.noreply.github.com>
2025-06-21 15:16:14 -0700

    add selection sort

--- slides/module_2/code/sorts.c
+++ slides/module_2/code/sorts.c
@@ -44,9 +44,50 @@ void ins_sort(int* arr, size_t n) {
         empty arrays are sorted by definition
 
     case P(n) -> P(n + 1):
-        Inductive Hypothesis: arr[..n] is sorted 
-        by correctness theorem of fast_insert, arr[..n+1] is sorted
+        Inductive Hypothesis: arr[..n) is sorted 
+        by correctness theorem of fast_insert, arr[..n+1) is sorted
 */
+
+// find the index of the smallest int in an array with length > 0
+int arg_min(int* arr, size_t n) {
+    assert(n > 0);
+
+    int the_min = arr[0];
+    int min_i = 0;
+
+    // invariant: the_min = min(arr[0..i))
+    //            min_i = arg_min(arr[0..i))
+    for (int i = 1; i < n; i++)
+        if (arr[i] < the_min) {
+            // update the min and min_i
+            the_min = arr[i];
+            min_i = i;
+        }
+        // the_min = min(arr[0..i), arr[i])
+        // min_i = arg_min(arr[0..i+1)), 
+    
+    // min_i = arg_min(arr[0..i+1))
+
+    return min_i;
+}
+
+// selection sort on array of size n
+void sel_sort(int* arr, size_t n) {
+    // base case: arr[0..0) is sorted
+    if (n == 0) return;
+
+    // arr[0..i) is sorted
+    for (int i = 0; i < n; i++) {
+        // add i because the arg_min will assume (arr + i) is index 0
+        int min_i = i + arg_min(arr + i, n - i);
+        // min_i = i + arg_min(arr[i..]) 
+        swap(arr + min_i, arr + i);
+        // everything in arr[0..i) <= arr[i] ->
+        // arr[0..i + 1) is sorted
+    }
+    
+    // arr[0..n) is sorted
+}
 
 void old_ins_sort(int* arr, size_t n) { 
     for (size_t i = 1; i < n; i++) 
@@ -54,7 +95,7 @@ void old_ins_sort(int* arr, size_t n) {
             swap(arr + j - 1, arr + j);
 }
 
-void do_unit_tests();
+void do_unit_tests(void);
 
 int main() {
     do_unit_tests();
@@ -66,33 +107,35 @@ int main() {
 #define mu_assert(message, test) \
     do { if (!(test)) return message; } while (0)
 
-#define mu_run(test) \
-    do { char *message = test(); tests_run++; \
+#define mu_run(test, sort) \
+    do { char *message = test(sort); tests_run++; \
          if (message) return message; } while (0)
 
-char* t_sort_empty() {
+typedef void (*sorter)(int* arr, size_t n);
+
+char* t_sort_empty(sorter sort) {
     int arr[3] = { 0xf00d1, 0xf00d2, 0xf00d3 };
-    ins_sort(arr + 1, 0);
+    sort(arr + 1, 0);
     mu_assert("empty sort touched elem -1", arr[0] == 0xf00d1);
     mu_assert("empty sort touched elem 0", arr[1] == 0xf00d2);
     mu_assert("empty sort touched elem 1", arr[2] == 0xf00d3);
     return 0;
 }
 
-char* t_sort_single() {
+char* t_sort_single(sorter sort) {
     int arr[1] = { 0xabcdef };
-    ins_sort(arr, 1);
+    sort(arr, 1);
     mu_assert("bad singleton sort", arr[0]==0xabcdef);
     return 0;
 }
 
-char* t_sort_pair() {
+char* t_sort_pair(sorter sort) {
     int arr[][2] = {
         {1, 2},
         {2, 1}
     };
-    ins_sort(arr[0], 2);
-    ins_sort(arr[1], 2);
+    sort(arr[0], 2);
+    sort(arr[1], 2);
     mu_assert("bad sort on in-order pair", arr[0][0] < arr[0][1]);
     mu_assert("bad sort on out-of-order pair", arr[1][0] < arr[1][1]);
     return 0;
@@ -105,7 +148,7 @@ int in_order(int* arr, size_t n) {
             return 0;
     return 1;
 }
-char* t_sort_triple() {
+char* t_sort_triple(sorter sort) {
     int arr[][3] = {
         {1, 2, 3},
         {1, 3, 2},
@@ -115,7 +158,7 @@ char* t_sort_triple() {
         {3, 2, 1},
     };
     for (int i = 0; i < 6; i++) {
-        ins_sort(arr[i], 3);
+        sort(arr[i], 3);
         sprintf(test_err_buf, "bad sort on triplet %d", i);
         mu_assert(test_err_buf, in_order(arr[i], 3));
     }
@@ -123,7 +166,7 @@ char* t_sort_triple() {
 }
 
 
-char* t_sort_random() {
+char* t_sort_random(sorter sort) {
     const int N_RANDOM = 1000;
     srand(0xcafef00d);
     
@@ -138,7 +181,7 @@ char* t_sort_random() {
             "sort failed on random array: %d %d %d %d %d %d %d %d %d",
             arr[0], arr[1], arr[2], arr[3], arr[4],
             arr[5], arr[6], arr[7], arr[8]);
-        ins_sort(arr, 9);
+        sort(arr, 9);
         mu_assert(test_err_buf, in_order(arr, 9));
     }
     
@@ -147,11 +190,17 @@ char* t_sort_random() {
 
 int tests_run = 0;
 char* run_tests() {
-    mu_run(t_sort_empty);
-    mu_run(t_sort_single);
-    mu_run(t_sort_pair);
-    mu_run(t_sort_triple);
-    mu_run(t_sort_random);
+    sorter sorts[] = { ins_sort, sel_sort };
+    char* sort_names[] = { "insertion sort", "selection sort" };
+
+    for (int i = 0; i < sizeof(sorts) / sizeof(sorter); i++) {
+        printf("testing '%s'\n", sort_names[i]);
+        mu_run(t_sort_empty, sorts[i]);
+        mu_run(t_sort_single, sorts[i]);
+        mu_run(t_sort_pair, sorts[i]);
+        mu_run(t_sort_triple, sorts[i]);
+        mu_run(t_sort_random, sorts[i]);
+    }
     return 0;
 }
 
